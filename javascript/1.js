98dd9bab8cc9b97ecbebeb27fe25f853d5afc718
freelancing with programming <iosattbsoft2021@gmail.com>
2023-09-29 18:55:11 +0500

    cc

--- javascript/1.js
+++ javascript/1.js
@@ -1,1 +1,9 @@
-console.log(Math.floor(Math.random()+100));
+let firstname = "Ali";
+let x = firstname;
+let n = 12;
+var n2 = 8;
+const PI = Math.PI;
+const someConstant  = 4;
+let name = "name";
+let message = `my name is ${name}`;
+console.log(message);
